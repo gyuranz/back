be9abcf8569ed6627779134b38fbce56793ead81
ThankYun <rlaehddbs2238@gmail.com>
2023-07-23 19:18:20 +0900

    주소변경

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,20 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Req } from '@nestjs/common';
 import { AppService } from './app.service';
 import { User } from './dtos&entitys/entity.entity';
 import { CreateRoomDto, JoinRoomDto } from './dtos&entitys/room.dto'
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
-  @Get('/:user_id')
-  async findUserInfoforMain(@Body() setDto: User){
-    return await this.appService.getUserInfoforMain(setDto.user_id);
+  @Get('/:user_id/finished')
+  async findUserInfoforMain(@Req() request) {
+    console.log(request);
+    const url = request.url;
+    const parts = url.split('/');
+    const user_id = parts[parts.length - 2];
+    console.log(user_id);
+    return await this.appService.getUserInfoforMain(user_id);
   }
 
   // @Post('/:user_id/join')
@@ -23,13 +28,13 @@ export class AppController {
   // }
 
   @Post('/:user_id/join')
-  async joinNewRoom(@Body() setDto: JoinRoomDto){
+  async joinNewRoom(@Body() setDto: JoinRoomDto) {
     return await this.appService.joinNewRoom(setDto)
   }
 
 
   @Post(':/user_id/create')
-  async createNewRoom(@Body() setDto: CreateRoomDto){
+  async createNewRoom(@Body() setDto: CreateRoomDto) {
     return await this.appService.createNewRoom(setDto);
   }
 }
