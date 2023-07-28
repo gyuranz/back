70b0c4d99b5c7525203fe9e3fb0dde3afb31b141
thankYun <rlaehddbs2238@gmail.com>
2023-07-29 03:56:06 +0900

    db 변경중

--- mongo-connection.js
+++ mongo-connection.js
@@ -1,5 +1,5 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://rlaehddbs2238:rlaehddbs123@cluster0.uusqfxm.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://gyuranz:0812@localhost:27017/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
